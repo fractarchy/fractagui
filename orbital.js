c62d90d79112402cdf60b4ecc98f974470c01075
e-teoria <33665826+e-teoria@users.noreply.github.com>
2019-09-17 13:50:42 +0200

    Add files via upload

--- orbital.js
+++ orbital.js
@@ -1966,10 +1966,6 @@ window.addEventListener("touchend", function (evt) {
     }
 }, false);
 
-document.addEventListener('touchstart', function(e) {
-    e.preventDefault();
-});
-
     /*
     var tmpim2 = document.getElementById("im");  
     var cnvim2 = document.createElement ("canvas");
